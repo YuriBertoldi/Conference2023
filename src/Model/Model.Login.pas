8649c5119a9675b0a1ad757389523785fd4466c6
Yuri Bulhões Bertoldi <ybulhoesbertoldi@gmail.com>
2023-08-21 20:58:02 -0300

    Teste remoção dos acentos

--- src/Model/Model.Login.pas
+++ src/Model/Model.Login.pas
@@ -19,20 +19,7 @@ type
 
 implementation
 
-////Teste unitario
-//procedure TestAuthenticateUser;
-//var
-//  Usuario: TUsuario;
-//  LoginResult: TLoginResult;
-//begin
-//  Usuario.Username := 'usuario';
-//  Usuario.Password := 'senha';
-//  LoginResult := TLogin.AuthenticateUser(Usuario);
-//  Assert(LoginResult = lrSuccess);
-//end;
-
-//Neste código, a autenticação de usuário é realizada de forma insegura, pois a senha e o nome de usuário estão hardcoded. Para melhorar a segurança, é recomendável armazenar as credenciais de usuário em um banco de dados e validar as credenciais de usuário contra o banco de dados. Além disso, é recomendável usar criptografia para armazenar as senhas no banco de dados.
-
+//<SECURITY>
 function TLogin.AuthenticateUser(const Usuario: TUsuario): TLoginResult;
 begin
   if (Usuario.Username = 'usuario' ) and (Usuario.Password = 'senha') then
@@ -40,7 +27,7 @@ begin
   else
     Result := lrInvalidCredentials;
 end;
-
+//</SECURITY>
 
 { TLoginResultHelper }
 
